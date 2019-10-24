47e7ebf87bdb2368119eea934f603966b98ea5e5
Thomas Goetten <thomas.goetten@gmail.com>
2019-10-24 07:27:47 +0200

    stack

--- aws_api_retrieve_office_endpoints/aws_api_retrieve_office_endpoints_stack.py
+++ aws_api_retrieve_office_endpoints/aws_api_retrieve_office_endpoints_stack.py
@@ -1,4 +1,8 @@
-from aws_cdk import core
+from aws_cdk import (
+    core,
+    aws_lambda as _lambda,
+    aws_apigateway as apigw,
+)
 
 
 class AwsApiRetrieveOfficeEndpointsStack(core.Stack):
@@ -6,4 +10,15 @@ class AwsApiRetrieveOfficeEndpointsStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # The code that defines your stack goes here
+        # Defines an AWS Lambda resource
+        lambda_retrieve_office_enpoints = _lambda.Function(
+            self, 'LambdaRetrieveOfficeEnpointsHandler',
+            runtime= _lambda.Runtime.PYTHON_3_7,
+            code= _lambda.Code.asset('lambda'), # directory of lambda function(s)
+            handler='retrieveofficeenpoints.handler',
+        )
+        
+        apigw.LambdaRestApi(
+            self, 'Endpoint',
+            handler=lambda_retrieve_office_enpoints,
+        )
